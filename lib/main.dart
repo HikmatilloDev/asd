c8f40e3b2be98d5614297ba647ce52dc683ffeaf
Succesfulperson <okhunov1819@gmail.com>
2022-08-15 16:58:51 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title:Text('Dicee'),
+      ),
+       body: DiceApp(),
+  ),
+  ));
+}
+
+class DiceApp extends StatefulWidget {
+ // const DiceApp({Key? key}) : super(key: key);
+
+  @override
+  State<DiceApp> createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int  leftImageNumber = 2;
+  int  rightImageNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children:<Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed:(){
+                  setState(){
+                    rightImageNumber = Random().nextInt(6)+1;
+                    leftImageNumber = Random().nextInt(6)+1;  //0.5
+                  };
+// print('left button num$leftImageNumber');
+                },
+                child: Image.asset('images/dice$leftImageNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(onPressed:(){
+                setState(){
+                  rightImageNumber = Random().nextInt(6)+1;
+                  leftImageNumber = Random().nextInt(6)+1;
+                };
+//  print('right button got pressed');
+              },
+                child: Image.asset('images/dice$rightImageNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
